382fdef8662c14e0645c97d6d4f083d64ac3b60b
Maryam-as <203218399+Maryam-as@users.noreply.github.com>
2025-04-02 22:01:55 +0330

    create styled div container for form controls using Styled Components and tagged templates

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -1,4 +1,13 @@
 import { useState } from "react";
+import { styled } from "styled-components";
+
+// create a styled div container for form controls
+const ControlContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  margin-bottom: 1.5rem;
+`;
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState("");
@@ -22,7 +31,7 @@ export default function AuthInputs() {
 
   return (
     <div id="auth-inputs">
-      <div className="controls">
+      <ControlContainer>
         <p>
           {/* dynamically setting the className for the label element based on the email validation status. */}
           <label className={`label ${emailNotValid ? "invalid" : ""}`}>
@@ -46,7 +55,7 @@ export default function AuthInputs() {
             }
           />
         </p>
-      </div>
+      </ControlContainer>
       <div className="actions">
         <button type="button" className="text-button">
           Create a new account
